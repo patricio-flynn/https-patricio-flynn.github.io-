73b3c913c4e2a510484e552e335fe80a20bfde62
patricio-flynn <127344435+patricio-flynn@users.noreply.github.com>
2023-03-30 16:13:39 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,20 @@
+
+let boton= document.getElementById("boton").addEventListener("click",function() {
+    document.getElementById("conocimientos").style.display="block";
+   
+});
+
+document.getElementById("conocimientos").style.display="none";
+
+document.getElementById('default').addEventListener('click', function() {
+  document.body.style.display="block";
+
+     });
+
+     document.getElementById("boton_ocultar").addEventListener("click", function() {
+     document.getElementById("conocimientos").style.display="none";
+
+     });
+
+
+  
